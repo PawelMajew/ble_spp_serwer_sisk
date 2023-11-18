3057036f4107a74c0d53650f3302c4477b7b1f07
Pawel <pawelma456@gmail.com>
2023-11-18 23:17:48 +0100

    first commit

---
+++ main/ble_spp_server.h
@@ -0,0 +1,50 @@
+
+#ifndef BLE_SPP_SERVER_H
+#define BLE_SPP_SERVER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+#include "driver/uart.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+
+
+#define spp_sprintf(s,...)         sprintf((char*)(s), ##__VA_ARGS__)
+#define SPP_DATA_MAX_LEN           (512)
+#define SPP_CMD_MAX_LEN            (20)
+#define SPP_STATUS_MAX_LEN         (20)
+#define SPP_DATA_BUFF_MAX_LEN      (2*1024)
+///Attributes State Machine
+enum{
+    SPP_IDX_SVC,
+
+    SPP_IDX_SPP_DATA_RECV_CHAR,
+    SPP_IDX_SPP_DATA_RECV_VAL,
+
+    SPP_IDX_SPP_DATA_NOTIFY_CHAR,
+    SPP_IDX_SPP_DATA_NTY_VAL,
+    SPP_IDX_SPP_DATA_NTF_CFG,
+
+    SPP_IDX_SPP_COMMAND_CHAR,
+    SPP_IDX_SPP_COMMAND_VAL,
+
+    SPP_IDX_SPP_STATUS_CHAR,
+    SPP_IDX_SPP_STATUS_VAL,
+    SPP_IDX_SPP_STATUS_CFG,
+    SPP_IDX_NB,
+};
+
+#endif /* BLE_SPP_SERVER_H */
